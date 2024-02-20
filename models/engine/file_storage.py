6d04c8185117ffd89528ccdb1925b81069a6ecd4
MMMBenchahyd <mohamedbenchahyd252@gmail.com>
2024-02-20 20:59:11 +0100

    check

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -45,19 +45,6 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                    self.all()[key] = classes[val['__class__']](**val)
+                        self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
-
-    def delete(self, obj=None):
-        """ Deletes an object from memory """
-        if obj is not None:
-            key = "{}.{}".format(type(obj).__name__, obj.id)
-            try:
-                del self.__objects[key]
-            except KeyError:
-                pass
-
-    def close(self):
-        """ reload() method for deserializing the JSON file to objects"""
-        self.reload()
