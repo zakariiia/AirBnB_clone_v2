cbc5739f534b595ac2e6ea03cc1d49c0ff1819df
MMMBenchahyd <mohamedbenchahyd252@gmail.com>
2024-02-22 00:33:30 +0100

    I'm dying

--- models/user.py
+++ models/user.py
@@ -15,3 +15,5 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
+    places = relationship("Place", backref="user",
+                        cascade='all, delete, delete-orphan')
