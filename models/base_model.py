39701b9969aca4f6d579ba3ea4633f73f22ba081
MMMBenchahyd <mohamedbenchahyd252@gmail.com>
2024-02-22 02:07:55 +0100

    I'm dying

--- models/base_model.py
+++ models/base_model.py
@@ -31,8 +31,6 @@ class BaseModel:
     def __str__(self):
         """Returns a string representation of the instance"""
         dictio = self.__dict__.copy()
-        if "_sa_instance_state" in dictio:
-            del dictio["_sa_instance_state"]
         return "[{}] ({}) {}".format(type(self).__name__, self.id, dictio)
 
     def save(self):
@@ -50,6 +48,8 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        if '_sa_instance_state' in dictionary.keys():
+            del dictionary['_sa_instance_state']
         return dictionary
     
     def delete(self):
