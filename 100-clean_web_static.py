d3b321d37da5f97f886201318a24b3c9853dbc2b
MMMBenchahyd <mohamedbenchahyd252@gmail.com>
2024-03-07 23:33:51 +0100

    deploy

---
+++ 100-clean_web_static.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Deletes out-of-date archives
+fab -f 100-clean_web_static.py do_clean:number=2
+    -i ssh-key -u ubuntu > /dev/null 2>&1
+"""
+
+import os
+from fabric.api import *
+
+env.hosts = ['18.207.3.171', '52.86.160.15']
+
+
+def do_clean(number=0):
+    """Delete out-of-date archives.
+
+    """
+    number = 1 if int(number) == 0 else int(number)
+
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(a)) for a in archives]
+
+    with cd("/data/web_static/releases"):
+        archives = run("ls -tr").split()
+        archives = [a for a in archives if "web_static_" in a]
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in archives]
