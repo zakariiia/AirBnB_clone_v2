a2b2fa7a12a11c4351d0b70fdccf1d8a4fd4b7c5
MMMBenchahyd <mohamedbenchahyd252@gmail.com>
2024-03-24 06:21:44 +0000

    task1&2

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-"""
-start Flask application
-"""
-
+""" a script that starts a Flask web application:"""
 from flask import Flask
+
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def index():
-    """returns Hello HBNB!"""
+@app.route('/',strict_slashes=False)
+def hello_hbnb():
+    """print Hello HBNB!"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """returns HBNB"""
+def HBNB():
+    """return HBNB"""
     return 'HBNB'
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
