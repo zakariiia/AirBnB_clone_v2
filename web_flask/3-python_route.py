47f2476f5558decfcbe7e03c399c3b9cebc10618
MMMBenchahyd <mohamedbenchahyd252@gmail.com>
2024-03-24 06:29:48 +0000

    task1&2

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -24,5 +24,12 @@ def cisfun(text):
     """display “C ” followed by the value of the text variable"""
     return 'C ' + text.replace('_', ' ')
 
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythoniscool(text='is cool'):
+    """display “Python ”, followed by the value of the text variable"""
+    return 'Python ' + text.replace('_', ' ')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
