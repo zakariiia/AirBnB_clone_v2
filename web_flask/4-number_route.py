e298cabe73ad9db0e0ca0bf607f3b432d1bca586
MMMBenchahyd <mohamedbenchahyd252@gmail.com>
2024-03-24 20:19:17 +0000

    4-number_route.py

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -33,11 +33,10 @@ def pythoniscool(text="is cool"):
     return "Python " + text
 
 
-@app.route("/number/<n>", strict_slashes=False)
+@app.route("/number/<int:n>", strict_slashes=False)
 def isnmber(n):
     """if n is a number print it"""
-    if int(n):
-        return f"{n} is a number"
+    return f"{n} is a number"
 
 
 if __name__ == "__main__":
