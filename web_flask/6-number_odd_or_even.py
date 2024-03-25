2ed483a031b81a161ff5e415028155d5b186d7e9
zakariiia <136108495+zakariiia@users.noreply.github.com>
2024-03-25 02:36:43 +0000

    Update 6-number_odd_or_even.py

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -8,19 +8,19 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def index():
-    """returns Hello HBNB!"""
+    """returning Hello HBNB!"""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """returns HBNB"""
+    """returning HBNB"""
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
 def cisfun(text):
-    """display “C ” followed by the <text>"""
+    """displays “C ” followed by the <text>"""
     txt = text.replace("_", " ")
 
     return "C " + txt
@@ -29,7 +29,7 @@ def cisfun(text):
 @app.route("/python/<text>", strict_slashes=False)
 @app.route("/python", strict_slashes=False)
 def pythoniscool(text="is cool"):
-    """display “Python ”, followed by the value of the text variable"""
+    """displays “Python ”, followed by the value of the text variable"""
     text = text.replace("_", " ")
     return "Python " + text
 
