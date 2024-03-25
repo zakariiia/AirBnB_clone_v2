e03d8ede21692aa7ca9206206a5140cf05b5b079
zakariiia <136108495+zakariiia@users.noreply.github.com>
2024-03-25 02:59:14 +0000

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Starts a Flask web application and display Hello HBNB!"""
+"""Start a Flask web application and display Hello HBNB!"""
 from flask import Flask
 
 app = Flask(__name__)
@@ -9,7 +9,6 @@ app = Flask(__name__)
 def hello_hbnb():
     """print Hello HBNB!"""
     return 'Hello HBNB!'
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
